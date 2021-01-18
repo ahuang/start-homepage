c2179b0c4c2293e61445ff216e1082de8221e099
ahuang <mengling915@163.com>
2021-01-18 14:19:06 +0800

    add: first commit

---
+++ .umirc.js
@@ -0,0 +1,9 @@
+import { join } from 'path';
+
+export default {
+  base: '/start-homepage/',
+  publicPath: '/start-homepage/',
+  plugins: [
+    'umi-plugin-gh-pages',
+  ],
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# test-gh-pages
+https://github.com/sorrycc/test-gh-pages
+https://sorrycc.github.io/test-gh-pages/
