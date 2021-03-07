9f2e02cb6737f0b791cd29946e811a2aca77d08f
ahuang <mengling915@163.com>
2021-03-07 11:07:36 +0800

    add: split data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import styles from './index.css';
-import data from './data.json';
+import news from './data/news.json';
+import search from './data/search.json';
+import tools from './data/tools.json';
+import study from './data/study.json';
+import life from './data/life.json';
+import fun from './data/fun.json';
+import vue from './data/fun.json';
+import react from './data/react.json';
+import wechat from './data/wechat.json';
+import alipay from './data/alipay.json';
 
 export default function() {
   const renderContent = () =>{
+    const data = [news,search, tools, study,life,fun,vue,react,wechat, alipay];
     const blocks = data.map(d =>{
       const list = d.list.map((m,i) =>{
         if(m.icon){
@@ -24,7 +34,6 @@ export default function() {
         </span>
         <>{list}</>
       </div>)
-
     });
     return blocks;
   }
